f65bc0ba23126481072b6edf6900586f8ac708e4
Javier495-lab <javierpenas025@gmail.com>
2024-11-13 15:28:09 +0100

    hnvhv

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public void Abrir()
+    {
+        Debug.Log("Abrir");
+    }
+
+    public void Cerrar()
+    {
+        Debug.Log("Cerrar");
+    }
+}
---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,44 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.tvOS;
+
+public class Interactor : MonoBehaviour
+{
+    LayerMask mask;
+    public float interactRange;
+    public TextMeshProUGUI leyenda;
+    RaycastHit hit;
+    void Start()
+    {
+        mask = LayerMask.GetMask("Raycast Detect");
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        if (Physics.Raycast(ray, out hit, interactRange, mask))
+        {
+            leyenda.gameObject.SetActive(true);
+            Door();
+        }
+        else
+        {
+            leyenda.gameObject.SetActive(false);
+        }
+    }
+
+    void Door()
+    {
+        if (hit.collider.CompareTag("Door"))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                hit.collider.transform.GetComponent<Door>().Abrir();
+            }
+            if (Input.GetMouseButtonDown(1))
+            {
+                hit.collider.transform.GetComponent<Door>().Cerrar();
+            }
+        }
+    }
+}
