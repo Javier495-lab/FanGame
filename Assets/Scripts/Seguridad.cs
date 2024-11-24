c27ab0ea6b44ba4ed1ba4e29556b43fb3f9a9506
Javier495 <9204644@alumnos.ufv.es>
2024-11-24 19:04:52 +0100

    Camaras

---
+++ Assets/Scripts/Seguridad.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Seguridad : MonoBehaviour
+{
+    public Canvas camaras;
+    public Camera[] secCamaras;
+    private Camera camaraActual;
+   
+    public void ActivarCamaras()
+    {
+        camaras.enabled = true;
+        secCamaras[0].enabled = false;
+        camaraActual = secCamaras[1];
+        camaraActual.enabled = true;
+    }
+
+    public void CambioCamara(int numeroCamara)
+    {
+        camaraActual.enabled = false;
+        camaraActual = secCamaras[numeroCamara];
+        camaraActual.enabled = true;
+    }
+
+    public void Volver()
+    {
+        camaras.enabled = false;
+        CambioCamara(0);
+    }
+}
