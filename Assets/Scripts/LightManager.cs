c1fadd78b53c90507873ba30609a2b7b8d8db073
Javier495 <javierpenas025@gmail.com>
2025-01-21 21:53:18 +0100

    Lues

---
+++ Assets/Scripts/LightManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    public Light []lucesCasa;
+    private bool encendido = true;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (GameManager.instance.dark)
+        {
+            foreach (Light i in lucesCasa)
+            {
+                i.enabled = false;
+                encendido = false;
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (!GameManager.instance.dark)
+        {
+            if (encendido)
+            {
+                GameManager.instance.SubPower(0.2f);
+                foreach (Light i in lucesCasa)
+                {
+                    i.enabled = false;
+                    encendido = false;
+                }
+            }
+            else if (!encendido)
+            {
+                GameManager.instance.AddPower(0.2f);
+                foreach (Light i in lucesCasa)
+                {
+                    i.enabled = true;
+                    encendido = true;
+                }
+            }
+        }
+    }
+}
