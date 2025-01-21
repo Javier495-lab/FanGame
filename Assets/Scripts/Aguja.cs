d5e88f1942dc4058d121551ac655a5285814d39f
Javier495-lab <javierpenas025@gmail.com>
2025-01-21 19:04:15 +0100

    Aguja arreglada

--- Assets/Scripts/Aguja.cs
+++ Assets/Scripts/Aguja.cs
@@ -4,12 +4,12 @@ using static UnityEngine.Rendering.DebugUI;
 public class Aguja : MonoBehaviour
 {
     [Range(0, 1)] private float rotacion;
-    private float minAngle = -146f;
-    private float maxAngle = 214f;
+    private float minAngle = 146f;
+    private float maxAngle = -146;
     void Update()
     {
         rotacion = GameManager.instance.power;
-        float angle = Mathf.Lerp(minAngle, maxAngle, 1f - rotacion);
+        float angle = Mathf.Lerp(minAngle, maxAngle, rotacion);
         transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, angle);
     }
 }
