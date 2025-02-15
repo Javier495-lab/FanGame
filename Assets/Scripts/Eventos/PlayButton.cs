3e388b5084ff2b7b396e145364bf562e21fc32bd
Javier495 <javierpenas025@gmail.com>
2025-02-15 19:50:12 +0100

    JapanWeekendDemo

--- Assets/Scripts/Eventos/PlayButton.cs
+++ Assets/Scripts/Eventos/PlayButton.cs
@@ -1,10 +1,26 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
 {
+    public GameObject normas;
+    private bool isPhoneGuy = false;
     public void Load()
     {
         SceneManager.LoadScene(1);
+    }
+    public void phoneGuy()
+    {
+        if (!isPhoneGuy)
+        {
+            isPhoneGuy = true;
+            normas.SetActive(true);
+        }
+        else
+        {
+            isPhoneGuy = false;
+            normas.SetActive(false);
+        }
     }
 }
